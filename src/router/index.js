61f6ec6981f4d1ef95c09b25ea2543984c1ef7f2
MD.Taraq Rahman <taraq.rahman@bol-online.com>
2025-06-26 12:27:18 +0600

    Added new vee validation package and added test page for testing

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import RegisterView from "@/views/RegisterView.vue";
 import ProfileView from "@/views/ProfileView.vue";
 import api from '@/axios'
+import TestView from "@/views/TestView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -30,6 +31,13 @@ const router = createRouter({
             name: 'profile',
             component: ProfileView,
             meta: {requiresAuth: true}
+        },
+
+        {
+            path: '/test',
+            name: 'test',
+            component: TestView,
+            meta: {guestOnly: true}
         },
     ],
 })
